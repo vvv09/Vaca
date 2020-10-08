f8f4f39b33177a094c53fc027e412ac0f330c94e
Vadim Valunskii <dvvv09@gmail.com>
2020-10-08 11:48:52 +0300

    Initial commit

---
+++ src/main/java/com/valunskii/vaco/VacoApplication.java
@@ -0,0 +1,13 @@
+package com.valunskii.vaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacoApplication.class, args);
+    }
+
+}
